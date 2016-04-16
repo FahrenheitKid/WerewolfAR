1f060cc23d4ba380c97e54a3fb77a39becab61a8
Jordan <jordan.silvaa42@gmail.com>
2016-04-16 17:02:37 -0300

    new models added, Press space to switch visible models

---
+++ Werewolf/Assets/VisionLogic.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionLogic : MonoBehaviour {
+
+
+	// Use this for initialization
+    public GameObject[] modelArray = new GameObject[3];
+    private int modelNumber;
+
+    
+    void Start()
+    {
+        modelNumber = 0;
+
+        for (int x = 0; x < modelArray.Length; x++)
+        {
+            if(x == 0)
+            modelArray[x] = GameObject.Find("Werewolf");
+
+            if (x == 1)
+                modelArray[x] = GameObject.Find("Villager");
+
+            if (x == 2)
+                modelArray[x] = GameObject.Find("Seer");
+
+
+
+
+        }
+        ModelSwitch();
+    }
+
+    void ModelSwitch()
+    {
+        for (int x = 0; x < modelArray.Length; x++)
+        {
+            if (x == modelNumber)
+            {
+                modelArray[x].SetActive(true);
+            }
+            else
+            {
+                modelArray[x].SetActive(false);
+            }
+        }
+        modelNumber += 1;
+        if (modelNumber > modelArray.Length - 1)
+        {
+            modelNumber = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ModelSwitch();
+        }
+    }
+}
