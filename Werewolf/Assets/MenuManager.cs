c67fd9f29ed2bd891a50ffd5adb0381b2ea56a5b
Mateus Ribeiro <mateussilvanh@hotmail.com>
2016-05-02 04:23:12 -0300

    menu

---
+++ Werewolf/Assets/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public InputField iField;
+    public static int GlobalNumberOfPlayer;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 + 50, 200, 50), "Start"))
+        {
+            GlobalNumberOfPlayer = int.Parse(iField.text);
+            
+            Debug.Log(GlobalNumberOfPlayer);
+
+            Application.LoadLevel(1);
+        }
+    }   
+}
